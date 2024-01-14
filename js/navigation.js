e401febc12e125f78673a4c6be6220a22a76faa9
AdrianGerman <adrian.german1019@gmail.com>
2024-01-14 09:08:30 -0600

    trending movies

--- js/navigation.js
+++ js/navigation.js
@@ -7,7 +7,7 @@ trendingBtn.addEventListener("click", () => {
 });
 
 arrowBtn.addEventListener("click", () => {
-  location.hash = "#home";
+  history.back();
 });
 
 window.addEventListener("DOMContentLoaded", navigator, false);
@@ -129,4 +129,7 @@ function trendsPage() {
   categoriesPreviewSection.classList.add("inactive");
   genericSection.classList.remove("inactive");
   movieDetailSection.classList.add("inactive");
+
+  headerCategoryTitle.innerHTML = "Tendencias";
+  getTrendingMovies();
 }
